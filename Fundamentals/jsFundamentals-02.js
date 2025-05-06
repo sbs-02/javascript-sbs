070c7d186741101fb47914f3279c4d9887551d26
sbs-02 <dangolsaksham8@gmail.com>
2025-05-06 18:39:49 +0545

    javascript

--- Fundamentals/jsFundamentals-02.js
+++ Fundamentals/jsFundamentals-02.js
@@ -20,3 +20,49 @@ console.log(fruitProcessor(5, 0));
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+//Function Declaration
+//You can call a Function Declaration before defining it
+function calcAge1(birthYear) {
+  const age = 2025 - birthYear;
+  return age;
+}
+const age1 = calcAge1(2002);
+console.log(`My age is ${age1} `);
+
+//Function Expression
+//You can't call a Function Declaration before initializing it
+const calcAge2 = function (birthYear) {
+  const age = 2025 - birthYear;
+  return age;
+};
+const age2 = calcAge2(1999);
+console.log(`My age is ${age2}`);
+
+//Practice
+//Function Declaration
+function recipe(ingredient1, ingredient2, ingredient3) {
+  const ingredients = `${ingredient1} , ${ingredient2} & ${ingredient3}`;
+  return ingredients;
+}
+const frenchToastRecipe = recipe("Eggs", "Milk", "Bread");
+console.log(`The ingredients for French Toast are ${frenchToastRecipe}.`);
+
+//Function Expression (same example)
+const Recipe = function (ingredient1, ingredient2, ingredient3) {
+  const ingredients = `${ingredient1}, ${ingredient2} & ${ingredient3}`;
+  return ingredients;
+};
+const ramenRecipe = Recipe("Noodles", "Water", "Seasoning");
+console.log(`The ingredients for Ramen are ${ramenRecipe}.`);
+
+//Arrow Function (Added is ES6)
+const calcAge3 = (birthYear) => 2025 - birthYear;
+const age3 = calcAge3(2002);
+console.log(age3);
+
+//Calling a function within a function
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
